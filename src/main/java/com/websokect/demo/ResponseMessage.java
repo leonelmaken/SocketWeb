ed838e1120edf21da63f4f4c2fe05e659de94126
leonelmaken <103751176+leonelmaken@users.noreply.github.com>
2023-08-24 11:26:03 +0100

    les webSockets tests

---
+++ src/main/java/com/websokect/demo/ResponseMessage.java
@@ -0,0 +1,21 @@
+package com.websokect.demo;
+
+public class ResponseMessage {
+
+	private String content;
+
+	public ResponseMessage(String content) {
+		super();
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
---
+++ src/test/java/com/websokect/demo/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websokect.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
